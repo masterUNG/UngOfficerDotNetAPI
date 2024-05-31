b263672324c1623098a6e58eb81fbf66732a4c42
มาสเตอร์ อึ่ง <phrombutr@gmail.com>
2024-06-01 05:57:27 +0700

    Start App

---
+++ android/app/src/main/kotlin/com/masterung/ungofficer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.masterung.ungofficer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/states/list_officer.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListOfficer extends StatelessWidget {
+  const ListOfficer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List Officer'),
+      ),
+    );
+  }
+}
